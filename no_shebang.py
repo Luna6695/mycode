d8726ea23d98335093fcfef8ba6281f6bb27268f
Lucy <lchen6695@gmail.com>
2022-03-07 15:09:26 +0000

    Our first commit

---
+++ no_shebang.py
@@ -0,0 +1,12 @@
+print ("Hello Team!") 
+
+
+'''Hello, this is for practice
+
+    Contact: the queen 
+    
+
+    Purpose: to receive some rice'''
+
+# display to standard out 
+print ('Hello') #Print() is a standard 
