df0c352977e313cc756a61bf7b3252c9335be966
Lucy <lchen6695@gmail.com>
2022-03-07 15:21:24 +0000

    Second Commit

---
+++ py3_shebang.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+
+Hello, I am back. Just typing along here. 
+
+Yes do not forget to sit tall. 
+
+Just practicing my typing. I need to work on it. I am so slow at typing. 
