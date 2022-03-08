79b250f8cda037065d9ac9e8f8f4555b9210105c
Lucy <lchen6695@gmail.com>
2022-03-08 15:16:38 +0000

    join and split

---
+++ strings/stringmethod02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author:RZFeeser@alta3.com"""
+
+def main():
+        """ Run-time code"""
+        # create a small string 
+        lilstring = "Alta3 Research offers classes on Python coding"
+        newlist = lilstring.split(" ") 
+        print(newlist)
+
+        myiplist = ["198", "168", "0", "12"]
+        singleip = ".".join(myiplist)
+        print(singleip)
+
+main()
